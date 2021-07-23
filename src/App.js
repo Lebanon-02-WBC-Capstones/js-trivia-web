48d430236c7cd6627ae0acdd87da94b8537eb0d8
raghadsam <raghad66samrout@gmail.com>
2021-07-23 20:48:45 +0300

    WhoAreWe and Report and Unpost modals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import React from "react";
+import WhoAreWe from "./Components/About/WhoAreWe";
+import Report from "./Components/Modals/Report";
+import Unpost from "./Components/Modals/Unpost";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
-        tadaaa
-      </header>
+      <WhoAreWe />
+      <Report />
+      <br /> <br />
+      <Unpost />
     </div>
   );
 }

d4a7368169a60f813b2bca19dff0beb4308f8537
wadad zein <wadad_zein@hotmail.com>
2021-07-24 11:19:11 +0300

    added Delete Question Modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import DeleteQuestion from "./components/Modals/DeleteQuestion";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
-      </header>
+      <DeleteQuestion />
     </div>
   );
 }
---
+++ src/components/Modals/DeleteQuestion.js
@@ -0,0 +1,44 @@
+import { React, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+import "./DeleteQuestion.css";
+
+export default function DeleteQuestion() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+  return (
+    <>
+      <button className="ModalButton" onClick={handleShow}>
+        Delete
+      </button>
+
+      <Modal
+        id="ModalPage"
+        show={show}
+        onHide={handleClose}
+        animation={false}
+      >
+        <Modal.Body bsPrefix="modal-body" className="Modals">
+          Delete this question?
+        </Modal.Body>
+        <Modal.Footer className="Modals">
+          <Button variant="outline-danger" className="Button" onClick={handleClose}>
+            Delete
+          </Button>{" "}
+          <Button
+            variant="outline-success"
+            className="Button"
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>{" "}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}

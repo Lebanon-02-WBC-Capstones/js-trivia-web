6a9364fbc46484aa7b919e71fe51525e40987c54
wadad zein <wadad_zein@hotmail.com>
2021-07-24 11:29:40 +0300

    added

---
+++ src/components/Modals/Cancel.js
@@ -0,0 +1,44 @@
+import { React, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+import "./Cancel.css";
+
+export default function DeleteQuestion() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+  return (
+    <>
+      <button className="ModalButton" onClick={handleShow}>
+        Cancel
+      </button>
+
+      <Modal
+        id="ModalPage"
+        show={show}
+        onHide={handleClose}
+        animation={false}
+      >
+        <Modal.Body bsPrefix="modal-body" className="Modals">
+          All your changes will be lost.
+        </Modal.Body>
+        <Modal.Footer className="Modals">
+          <Button variant="outline-danger" className="Button" onClick={handleClose}>
+            Okay
+          </Button>{" "}
+          <Button
+            variant="outline-success"
+            className="Button"
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>{" "}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}

dec0b3c0e5b87608915c6cccc010fcafecf7490b
wadad zein <wadad_zein@hotmail.com>
2021-07-24 14:59:00 +0300

    Added Message Modal

---
+++ src/components/Modals/Message.js
@@ -0,0 +1,32 @@
+import { React, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+import "./Message.css";
+
+export default function Unpost() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <button className="ModalButton" onClick={handleShow}>
+        notification
+      </button>
+
+      <Modal id="ModalPage" show={show} onHide={handleClose} animation={false}>
+        <Modal.Title className="Modals">
+          This quiz was unposted by the admin.
+        </Modal.Title>
+        <Modal.Body bsPrefix="modal-body" className="Modals">
+       Your quiz has some inaccurate or inappropriate content.
+        </Modal.Body>
+        <Modal.Footer className="Modals">
+         <b>Edit your quiz to be able to repost it.</b>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}

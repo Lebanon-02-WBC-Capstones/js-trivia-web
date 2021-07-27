6ab3efffe00f95f562dec2d585bcc7403d7b13dc
Salim Al Mersally <salim.almersally@lau.edu>
2021-07-27 17:14:13 +0300

    Made Report table

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
 import React from "react";
-import Home from './components/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-  
     <div className="App">
       <div className="page-container">
         <div className="content-wrap">
-         
-
-    <Home/>
-    </div>
-    </div>
+          <Home />
+        </div>
+      </div>
     </div>
   );
 }

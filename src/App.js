dbc93bc26d877de161c5468f8d7dc7ded131168e
Hazem El Sankari <hazemleb96@gmail.com>
2021-07-28 23:07:37 +0300

    Adjusted Team Card and Grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import "./App.css";
 import React from "react";
-import WhoAreWe from "./Components/About/WhoAreWe";
-import Report from "./Components/Modals/Report";
-import Unpost from "./Components/Modals/Unpost";
+// import WhoAreWe from "./Components/About/WhoAreWe";
+// import Report from "./Components/Modals/Report";
+// import Unpost from "./Components/Modals/Unpost";
 import About from "./Components/About";
-import Sign_in from "./Components/Modals/Sign_in";
-import Home from "./Components/Home";
+// import Sign_in from "./Components/Modals/Sign_in";
+// import Home from "./Components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home />
       <WhoAreWe />
-      <Report />
+      <Report /> */}
       <br /> <br />
-      <Unpost />
+      {/* <Unpost /> */}
       <p>Sign In Below</p>
-      <Sign_in />
+      {/* <Sign_in /> */}
       <About />
     </div>
   );

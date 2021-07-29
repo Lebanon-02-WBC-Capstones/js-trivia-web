17d0fecb87e0660e9ee46a575ec2972af1a0169d
Salim Al Mersally <salim.almersally@lau.edu>
2021-07-29 15:40:23 +0300

    created search box

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import React from "react";
 // import WhoAreWe from "./Components/About/WhoAreWe";
 // import Report from "./Components/Modals/Report";
 // import Unpost from "./Components/Modals/Unpost";
-import About from "./Components/About";
-import Footer from "./Components/Footer/Footer";
-import Sign_in from "./Components/Modals/Sign_in";
+// import About from "./Components/About";
+// import Footer from "./Components/Footer/Footer";
+// import Sign_in from "./Components/Modals/Sign_in";
 // import Home from "./Components/Home";
+// import Sign_up from "./Components/Modals/Sign_up";
+// import Quizes from "./Components/Quizes";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Sign_up from "./Components/Modals/Sign_up";
+
 function App() {
   return (
     <div className="App">
-      <Footer />
-      {/* <Home />
+      {/* <Footer />
+      <Home />
       <WhoAreWe />
-      <Report /> */}
+      <Report />
       <br /> <br />
-      {/* <Unpost /> */}
+      <Unpost />
       <p>Sign In Below</p>
       <Sign_in />
       <Sign_up />
-      <About />
+      <About /> */}
+      {/* <Quizes /> */}
     </div>
   );
 }

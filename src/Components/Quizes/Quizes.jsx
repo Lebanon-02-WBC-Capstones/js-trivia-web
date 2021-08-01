24fdfd1d8d6046ba0b6b0a0bf7eb11ba6dbb2c60
Hazem El Sankari <hazemleb96@gmail.com>
2021-08-01 22:34:09 +0300

    Continuing Work with Quiz Cards after resolving conflicts

--- src/Components/Quizes/Quizes.jsx
+++ src/Components/Quizes/Quizes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Filters from "../Filters/Filters";
-import SearchBar from "./SearchBar";
+// import Filters from "../Filters/Filters";
+// import SearchBar from "./SearchBar";
+// import QuizGrid from "./QuizGrid";
 function Quizes() {
   return (
     <div className="Row container">
-      <Filters />
-      <SearchBar />
+      {/* <Filters />
+      <SearchBar /> */}
+      {/* <QuizGrid /> */}
     </div>
   );
 }
---
+++ src/Components/Quizes/Quizzes.js
@@ -0,0 +1,59 @@
+export const categories = {
+  0: "history",
+  1: "science",
+  2: "music",
+  3: "geography",
+  4: "languages",
+  5: "coding",
+  6: "literature",
+  7: "movies",
+  8: "business",
+  9: "politics",
+  10: "sports",
+  11: "challenges",
+};
+
+export const difficulty = {
+  1: "Easy",
+  2: "Med",
+  3: "Hard",
+};
+
+export const quizzes = [
+  {
+    name: "Quiz Name",
+    rating: 3.8,
+    difficulty: 1,
+    players: 150,
+  },
+  {
+    name: "Quiz Name",
+    rating: 3.8,
+    difficulty: 2,
+    players: 150,
+  },
+  {
+    name: "Quiz Name",
+    rating: 3.8,
+    difficulty: 3,
+    players: 100,
+  },
+  {
+    name: "Quiz Name",
+    rating: 2.1,
+    difficulty: 2,
+    players: 150,
+  },
+  {
+    name: "Quiz Name",
+    rating: 3.8,
+    difficulty: 2,
+    players: 500,
+  },
+  {
+    name: "Quiz Name",
+    rating: 5.0,
+    difficulty: 3,
+    players: 200,
+  },
+];

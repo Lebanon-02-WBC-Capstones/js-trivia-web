99136177f68d16e79e4d7ebc9544acf87b4531fe
Hazem El Sankari <hazemleb96@gmail.com>
2021-08-02 22:28:10 +0300

    Removed Team Member Images + Edited Styling for Quiz Cards

--- src/Components/Quizes/QuizCard.jsx
+++ src/Components/Quizes/QuizCard.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { difficulty, categories } from "./Quizzes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../App.css";
-// import { ReactSVG } from "react-svg";
-// import starsvg from "./starsvg.svg";
 
 // Please don't remove the comments; they help in breaking down the code and
 // organizing it
@@ -12,26 +10,38 @@ function QuizCard(props) {
   function diff_color(quiz) {
     if (quiz.difficulty === 1) {
       return (
-        <span className="badge rounded-pill bg-success align-left">
+        <span
+          className="badge rounded-pill bg-success align-left"
+          style={{ fontSize: "0.5rem" }}
+        >
           {difficulty[quiz.difficulty]}
         </span>
       );
     } else if (quiz.difficulty === 3) {
       return (
-        <span className="badge rounded-pill bg-danger align-left">
+        <span
+          className="badge rounded-pill bg-danger align-left"
+          style={{ fontSize: "0.5rem" }}
+        >
           {difficulty[quiz.difficulty]}
         </span>
       );
     } else {
       return (
-        <span className="badge rounded-pill bg-warning align-left">
+        <span
+          className="badge rounded-pill bg-warning align-left"
+          style={{ fontSize: "0.5rem" }}
+        >
           {difficulty[quiz.difficulty]}
         </span>
       );
     }
   }
   return (
-    <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
+    <div
+      className="row row-cols-1 row-cols-md-2 g-4 justify-content-center"
+      style={{ fontSize: "0.5rem" }}
+    >
       {props.quizzes.map((quiz, index) => {
         return (
           <div className="col-6 col-md-6" key={index}>
@@ -47,39 +57,23 @@ function QuizCard(props) {
                     <div className="col-4 col-md-4 d-flex ">
                       <span className="star">&#9733;</span>
                       <p className="cardText ">{quiz.rating}</p>
-                      {/* <div className="row justify-content-center">
-                        <div className="col-3 col-md-3 pb-5 ">
-                          <img src={star} width="10px" />
-                        </div>
-                        <div className="col-3 col-md-3 p-0">
-                          <p className="cardText ">{quiz.rating}</p>
-                        </div>
-                      </div> */}
                     </div>
                   </div>
                   {/* End of First Row */}
-
                   {/* Start of Second Row */}
-                  <div className="row justify-content-start ml-3">
-                    <div className="col-2 col-md-4">
-                      {/* {if(quiz.difficulty === 1){
-                        return (<span className="badge rounded-pill bg-success align-left">
-                          {difficulty[quiz.difficulty]}
-                        </span>)}
-                      } */}
-                      {/* <span className="badge rounded-pill bg-success align-left">
-                        {difficulty[quiz.difficulty]}
-                      </span> */}
-                      {diff_color(quiz)}
-                    </div>
+                  <div className="row justify-content-start">
+                    <div className="col-4 col-md-4">{diff_color(quiz)}</div>
                   </div>
                   {/* End of Second Row */}
                   {/* Start of Third Row */}
                   <div className="row justify-content-start">
-                    <div className="col-6 col-md-6">
-                      <p className="cardText">Players: {quiz.players}</p>
+                    <div className="col-6 col-md-6 justify-content-start">
+                      <p className="cardText align-left">
+                        Players: {quiz.players}
+                      </p>
                     </div>
                   </div>
+                  {/* End of Third Row */}
                 </div>
               </div>
             </div>

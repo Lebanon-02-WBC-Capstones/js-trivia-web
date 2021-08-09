bdf1a71fa1153e8fcbb545ef2b947a36e198c843
Abdulrahman Y. idlbi <adlogi@alum.mit.edu>
2021-08-09 21:30:08 +0300

    style: yarn format

--- src/components/Modals/Rating.jsx
+++ src/components/Modals/Rating.jsx
@@ -4,8 +4,8 @@ import Modal from "react-bootstrap/Modal";
 //import {  } from "react-bootstrap-icons";
 
 function Rating(props) {
-     const [rating, setRating] = useState(0);
-     const [show, setShow] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -14,25 +14,24 @@ function Rating(props) {
     handleClose();
   };
 
-    return (
-        <>
-        {/* <button
+  return (
+    <>
+      {/* <button
         className="button btn-outline-danger col-2 m-1"
         onClick={handleShow}
       >
        rate me
       </button> */}
 
-        <Modal id="ModalPage" show={show} onHide={handleClose}  animation={false}>
+      <Modal id="ModalPage" show={show} onHide={handleClose} animation={false}>
         <Modal.Body bsPrefix="modal-body" className="Modals">
-         Rate this quiz
-        <Rate rating={rating} onRating={(rate) => setRating(rate)} />
-        <p>Rating - {rating}</p>
+          Rate this quiz
+          <Rate rating={rating} onRating={(rate) => setRating(rate)} />
+          <p>Rating - {rating}</p>
         </Modal.Body>
-        
       </Modal>
     </>
-    );
+  );
 }
 
 export default Rating;

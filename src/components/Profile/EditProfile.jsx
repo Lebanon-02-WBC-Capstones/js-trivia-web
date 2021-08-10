b5bfd7a3a7c58797de71c38e87de86afee8cc0ed
Omar Nachar <omarnachar2@gmail.com>
2021-08-10 05:06:22 -0700

    Profile Edit

---
+++ src/components/Profile/EditProfile.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Image, Button } from "react-bootstrap";
+import DefaultPic from "../../assets/user-profile.png";
+import EditProfileModal from "../Modals/EditProfileModal";
+
+export default function EditProfile() {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <Image src={DefaultPic} width="193" roundedCircle />
+      <h1>Welcome User</h1>
+      <Button
+        className="edit-profile-button"
+        variant="text"
+        style={{ color: "blue" }}
+        onClick={() => setShow(true)}
+      >
+        <i>edit profile</i>
+      </Button>
+      <EditProfileModal show={show} onHide={() => setShow(false)} />
+    </div>
+  );
+}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReportsTable from "./ReportsTable";
+import EditProfile from "./EditProfile";
 
 function Profile() {
-  return <ReportsTable />;
+  return (
+    <div>
+      <EditProfile />
+      <ReportsTable />
+    </div>
+  );
 }
 
 export default Profile;

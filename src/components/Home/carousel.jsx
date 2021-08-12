f445ad5578a07b1631c8ddece310231fe2ba8194
Hazem El Sankari <hazemleb96@gmail.com>
2021-08-12 12:23:03 +0300

    Constrain the carousel on landing to have fixed height and width Fixes #79

--- src/components/Home/carousel.jsx
+++ src/components/Home/carousel.jsx
@@ -1,33 +1,30 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import "./home.css";
-
-import img3 from "../../assets/kids.png";
-// import img2 from "../../assets/remote.png";
-// import img1 from "../../assets/team.png";
+import carimg1 from "../../assets/pictures/carouselImg1.jpg";
+import carimg2 from "../../assets/pictures/carouselImg2.gif";
+import carimg3 from "../../assets/pictures/carouselImg3.png";
 
 function CarouselMain() {
   return (
-    <Carousel className="carousel" controls={false} fade={true} pause={false}>
-      <Carousel.Item interval={2000}>
-        <img
-          className="carouselImg"
-          src="https://cdn.nohat.cc/thumb/f/720/fd185374d03b4664bcbb.jpg"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img
-          className="carouselImg"
-          src="https://cdn.dribbble.com/users/623359/screenshots/13984999/media/cca4371fa48bf2df483d879b572870cd.png"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img
-          className="carouselImg"
-          src="https://cdn.dribbble.com/users/623359/screenshots/13988957/media/619eed2ffa166f24096742e07d97f63a.png"
-        />
-      </Carousel.Item>
-    </Carousel>
+    <div className="carouselDiv mx-auto mt-4">
+      <Carousel
+        className=" mx-auto carouselSize"
+        controls={false}
+        fade={true}
+        pause={false}
+      >
+        <Carousel.Item interval={5000}>
+          <img className="carouselImg" src={carimg1} />
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img className="carouselImg" src={carimg2} />
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img className="carouselImg" src={carimg3} />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 

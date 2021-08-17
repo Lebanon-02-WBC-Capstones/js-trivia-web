513c4ee0f891e923617ede7715bbc052520e76cc
Salim Al Mersally <salim.almersally@lau.edu>
2021-08-17 17:53:39 +0300

    Activate the buttons on the result page Fixes #128

--- src/components/Result/ResultButtons.jsx
+++ src/components/Result/ResultButtons.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 function ResultButtons() {
+  const location = useLocation();
+
   return (
     <div className="row justify-content-between p-5 text-center">
       <div className="col-6 col-md-6">
-        <Button id="SignInButton">Play Again</Button>
+        <Link
+          to={"/question/" + location.state.quizId}
+          style={{ all: "unset" }}
+        >
+          <Button id="SignInButton">PlayAgain</Button>
+        </Link>
       </div>
       <div className="col-6 col-md-6">
-        <Button id="SignInButton">Done</Button>
+        <Link to="/" style={{ all: "unset" }}>
+          <Button id="SignInButton">Done</Button>
+        </Link>
       </div>
     </div>
   );
